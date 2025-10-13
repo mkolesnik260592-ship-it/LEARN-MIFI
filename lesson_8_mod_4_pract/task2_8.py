936fd780e159d7020fd7bdd5208f51907682c636
Matthew <mkolesnik260592@gmail.com>
2025-10-13 17:33:04 +0300

    Обновил данные

--- lesson_8_mod_4_pract/task2_8.py
+++ lesson_8_mod_4_pract/task2_8.py
@@ -6,14 +6,10 @@ def reverse_image_recursive(image):
     """Рекурсивное преобразование в негатив."""
     if not image:
         return []
-
-    first_row = image[0]  # Берем первую строку
-    rest_of_image = image[1:]  # Берем остальную часть изображения
-    # Инвертируем первую строку (можно использовать списковое включение)
+    first_row = image[0]
+    rest_of_image = image[1:]
     inverted_first_row = [1 - pixel for pixel in first_row]
-    # Рекурсивно инвертируем остальную часть изображения
     inverted_rest_of_image = reverse_image_recursive(rest_of_image)
-    # Объединяем инвертированную первую строку с инвертированной остальной частью
     return [inverted_first_row] + inverted_rest_of_image
 
 # Пример входных данных
